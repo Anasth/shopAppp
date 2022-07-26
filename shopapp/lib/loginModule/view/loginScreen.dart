433f6bd819ce156fc9ed7b4442dbb1706ace7102
Anasth <69089332+Anasth@users.noreply.github.com>
2022-07-26 21:03:40 +0200

    first commit

---
+++ shopapp/lib/loginModule/view/loginScreen.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class LogInScreen extends StatelessWidget {
+  const LogInScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("home")),
+      body: Text("data"),
+    );
+  }
+}
